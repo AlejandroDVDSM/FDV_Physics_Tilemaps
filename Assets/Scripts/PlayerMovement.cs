07380b26df1800bf2097462e4689a64d488994cb
AlejandroDVDSM <bysalexx@gmail.com>
2024-10-19 12:38:06 +0100

    Complete physics exercises

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,33 +10,49 @@ public class PlayerMovement : MonoBehaviour
 
     private SpriteRenderer _spriteRenderer;
     private Animator _animator;
+    private Rigidbody2D _rigidbody2D;
     
     private static readonly int IsWalking = Animator.StringToHash("isWalking");
-    private static readonly int VerticalMovement = Animator.StringToHash("verticalMovement");
 
     private void Start()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
         _horizontalMovement = Input.GetAxisRaw("Horizontal");
-        _verticalMovement = Input.GetAxisRaw("Vertical");
 
-        _animator.SetFloat(VerticalMovement, _verticalMovement);
-        if (_horizontalMovement != 0 || _verticalMovement != 0)
+        if (_horizontalMovement != 0)
         {
             _animator.SetBool(IsWalking, true);
             _spriteRenderer.flipX = _horizontalMovement > 0;
             
-            _direction = new Vector3(_horizontalMovement, _verticalMovement, transform.position.z).normalized;
-            transform.Translate(_direction * (_moveSpeed * Time.deltaTime));
+            _direction = new Vector3(_horizontalMovement, 0, 0).normalized;
         }
         else
         {
             _animator.SetBool(IsWalking, false);
         }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_horizontalMovement != 0)
+        {
+            _rigidbody2D.MovePosition(transform.position + _direction * (_moveSpeed * Time.fixedDeltaTime));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log($"<{gameObject.name}> has collided with <{other.gameObject.name}>");
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log($"<{gameObject.name}> triggered");
     }
 }
